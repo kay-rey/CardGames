c3a2c1154f6ff3179edceeb0c07e8c1cb1e1022d
Kevin Baltazar Reyes <21996242+kay-rey@users.noreply.github.com>
2025-03-26 14:35:54 -0700

    implemented a player input function

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,10 @@
 from card import Card, createDeck
 from random import shuffle #for shuffle() on the lists
 
+DEALER_STAND_NUMBER: int = 17
+PLAYER_TURN = True
+DEALER_TURN = False
+
 def dealACard(player: list[Card], deck: list[Card]):
     player.append(deck.pop())
 
@@ -15,14 +19,25 @@ def playerTotals(player_hand: list[Card]) -> int:
             total += 10
     return total
 
+def getPlayerInput() -> str:
+    playerInputLowered: str = ''
+    while True:
+        playerInput = input('Would you like to [H]it or [S]tand?')
+        if isinstance(playerInput, str):
+            playerInputLowered: str = playerInput.lower()
+            if playerInputLowered in ['h', 's']:
+                return playerInputLowered
+        print('Input a valid option')
+
+
 def playBlackjack(num_seats: int, num_decks: int):
     players: list[list[Card]] = []
-    playerTotal = 0
-    dealerTotal = 0
+    playerTotal: int = 0
+    dealerTotal: int = 0
     playerHand: list[Card] = []
     dealerHand: list[Card] = []
     while True:
-        decks = createDeck(1)
+        decks: list[Card] = createDeck(1)
         shuffle(decks)
         print('Dealing the cards...')
         for i in range(2):
@@ -36,8 +51,12 @@ def playBlackjack(num_seats: int, num_decks: int):
         if dealerTotal == 21:
             print('The dealer hit blackjack with a hand of ' + str(playerHand[0]) + ' and ' + str(playerHand[1]) + '. Better luck next time')
             break
-        print(f'The dealer has a hand total of {str(dealerTotal)} with a hand of {dealerHand[0]} and {dealerHand[1]}')
+        print(f'The dealer revealed a {dealerHand[0]}')
         print(f'You have a hand total of {str(playerTotal)} with a hand of {playerHand[0]} and {playerHand[1]}')
+        while PLAYER_TURN:
+            playerInput: str = getPlayerInput()
+            print('You chose ' + playerInput)
+
 
 
         break
