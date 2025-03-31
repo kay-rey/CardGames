1cbbe8d34e886778c6b476fcdccc393f47da2285
Kevin Baltazar Reyes <21996242+kay-rey@users.noreply.github.com>
2025-03-30 21:54:01 -0700

    just need to turn hands into a dictionary

--- blackjack.py
+++ blackjack.py
@@ -1,17 +1,19 @@
-from card import Card, Deck
+from card import Deck
 from player import Player, handTotals, dealACard
 
 DEALER_STAND_NUMBER: int = 17
+SPLIT_HAND_COUNT: int = 0
 BLACKJACK: int = 21
-TOTAL_BET: int = 0
-GAME_DECK: list[Card]
+INITIAL_BET: int = 0
+GAME_DECK: Deck
 PLAYER_TURN: bool
+HANDS_IN_PLAY: dict = {}
 
 
 def hitOrStandPlayerInput() -> str:
     while True:
         try:
-            playerInput = int(input('Would you like to [1]Hit, [2]Stand, [3]Double Down, or [4]Split?\n'))
+            playerInput = int(input('Would you like to [1]Hit, [2]Stand, or [3]Double Down?\n'))
             if isinstance(playerInput, int):
                 if playerInput == 1:
                     return 'HIT'
@@ -19,8 +21,6 @@ def hitOrStandPlayerInput() -> str:
                     return 'STAND'
                 elif playerInput == 3:
                     return 'DOUBLEDOWN'
-                elif playerInput == 4:
-                    return 'SPLIT'
             print('That\'s not a valid option')
         except ValueError:
             print('Enter a valid number')
@@ -44,8 +44,42 @@ def splitOrHitOrStand() -> str:
             print('Enter a valid number')
 
 
-def splitGameplay(hand: list[Card], bank: int):
-    pass
+def splitGameplay(player: Player, player_bank: Player):
+    global PLAYER_TURN, INITIAL_BET, SPLIT_HAND_COUNT, HANDS_IN_PLAY
+    currBet: int = INITIAL_BET
+    dealACard(player.hand, GAME_DECK)
+    while True:
+        if player.hand[0].number == player.hand[1].number:
+            playerInput: str = splitOrHitOrStand()
+        else:
+            playerInput: str = hitOrStandPlayerInput()
+        print('You chose to ' + playerInput)
+        if playerInput == 'HIT':
+            dealACard(player.hand, GAME_DECK)
+            playerTotal = handTotals(player)
+            print(f'You were dealt a {player.hand[-1]}, your total is now {playerTotal}')
+            if playerTotal > BLACKJACK:
+                print('You busted! Bye Bye!!')
+                player_bank.subtractMoney(currBet)
+                break
+        elif playerInput == 'STAND':
+            HANDS_IN_PLAY = {player: currBet}
+            PLAYER_TURN = False
+        elif playerInput == 'DOUBLEDOWN':
+            totalBet = currBet * 2
+            dealACard(player.hand, GAME_DECK)
+            print(f'You were dealt a {player.hand[-1]}')
+            if handTotals(player) > BLACKJACK:
+                print('You BUSTED. Better luck next time')
+                player.subtractMoney(totalBet)
+                break
+            HANDS_IN_PLAY = {player: currBet}
+            PLAYER_TURN = False
+        elif playerInput == 'SPLIT':
+            SPLIT_HAND_COUNT += 1
+            extraHand = Player('split hand: ' + SPLIT_HAND_COUNT, 0, [player.hand.pop()])
+            splitGameplay(extraHand, player_bank)
+            splitGameplay(player, player_bank)
 
 
 def playerOutcome(player_total: int, dealer_total: int) -> str:
@@ -58,20 +92,18 @@ def playerOutcome(player_total: int, dealer_total: int) -> str:
 
 
 def playBlackjack(player: Player, dealer: Player, decks: Deck):
-    global GAME_DECK
-    global PLAYER_TURN
+    global GAME_DECK, PLAYER_TURN, SPLIT_HAND_COUNT, INITIAL_BET, HANDS_IN_PLAY
     playerTotal: int
     dealerTotal: int
-    initialBet: int
     totalBet: int
-    splitHandCount: int = 0
+    SPLIT_HAND_COUNT = 0
     GAME_DECK = decks
     PLAYER_TURN = True
     print(f'You have ${player.money} in the bank')
     while True:  # loop starts the betting
         try:
-            initialBet = int(input('How much would you like to bet?\n'))
-            totalBet = initialBet
+            INITIAL_BET = int(input('How much would you like to bet?\n'))
+            totalBet = INITIAL_BET
             if 1 <= totalBet <= player.money:
                 print(f'You bet {totalBet}')
                 break
@@ -113,43 +145,46 @@ def playBlackjack(player: Player, dealer: Player, decks: Deck):
                     player.subtractMoney(totalBet)
                     break
             elif playerInput == 'STAND':
+                HANDS_IN_PLAY = {player: totalBet}
                 PLAYER_TURN = False
             elif playerInput == 'DOUBLEDOWN':
-                totalBet = initialBet * 2
+                totalBet = INITIAL_BET * 2
                 dealACard(player.hand, GAME_DECK)
                 print(f'You were dealt a {player.hand[-1]}')
                 if handTotals(player) > BLACKJACK:
                     print('You BUSTED. Better luck next time')
                     player.subtractMoney(totalBet)
                     break
+                HANDS_IN_PLAY = {player: totalBet}
                 PLAYER_TURN = False
             elif playerInput == 'SPLIT':
-                splitHandCount += 1
-                extraHand = Player('split hand : ' + splitHandCount, 0, [player.hand.pop()])
-                splitGameplay(extraHand.hand, player.money)
-                splitGameplay(player.hand, player.money)
-
-                pass
+                SPLIT_HAND_COUNT += 1
+                extraHand = Player('split hand : ' + SPLIT_HAND_COUNT, 0, [player.hand.pop()])
+                splitGameplay(extraHand, player)
+                splitGameplay(player, player)
         while not PLAYER_TURN:
             print(
                 f'The dealer reveals their second card: {dealer.hand[-1]}. Their hand total is {handTotals(dealer)}')
             while handTotals(dealer) < 17:
                 dealACard(dealer.hand, GAME_DECK)
                 print(f'The dealer drew a {dealer.hand[-1]}. Their total is now {handTotals(dealer)}')
-            playerOutcomeCondition = playerOutcome(handTotals(player), handTotals(dealer))
             if handTotals(dealer) > BLACKJACK:
                 print('The dealer BUSTED. You win!')
                 player.addMoney(totalBet)
                 break
-            if playerOutcomeCondition == 'PUSH':
-                print('Push! No one wins')
-                break
-            elif playerOutcomeCondition == 'WIN':
-                print('You WON!! Congrats')
-                player.addMoney(totalBet)
-                break
-            else:
-                print('You lost! The house always wins')
-                player.subtractMoney(totalBet)
-                break
+
+            # TODO: turn hands into a dictionary
+            for hand, bet_amount in HANDS_IN_PLAY.items():
+                playerOutcomeCondition = playerOutcome(handTotals(player), handTotals(dealer))
+                if playerOutcomeCondition == 'PUSH':
+                    print('Push! No one wins')
+                    break
+                elif playerOutcomeCondition == 'WIN':
+                    print('You WON!! Congrats')
+                    player.addMoney(totalBet)
+                    break
+                else:
+                    print('You lost! The house always wins')
+                    player.subtractMoney(totalBet)
+                    break
         break
